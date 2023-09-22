1b7024a50358d61a49a2464f80eb6dfeb1276ef3
student <a0025942@gmail.com>
2023-09-22 09:15:56 +0200

    commit message

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         //
+        findViewById<Button>(R.id.PrzyciskKot)
+        findViewById<ImageView>(R.id.Kot).setImageResource(
+            R.drawable.pobrane
+        )
+
+        }
     }
 }
